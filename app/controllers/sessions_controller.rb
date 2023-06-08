1f0ec7724d7d29f947472dc16c87e9a8082503ce
Ali Eyvazli <ali.eyvazli@kellogg.northwestern.edu>
2023-06-08 00:13:32 +0000

    step2/1

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,16 @@
-# app/controllers/sessions_controller.rb
-
 class SessionsController < ApplicationController
   def new
   end
 
   def create
-    # TODO: authenticate user
+    user = User.find_by(email: params[:email])
+    if user.nil?
+      redirect_to login_path, flash: { error: "Invalid email or password" }
+    elsif user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to posts_path, flash: { notice: "Logged in successfully" }
+    else
+      redirect_to login_path, flash: { error: "Invalid email or password" }
+    end
   end
 end
